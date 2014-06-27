9ef8ba00fcd9952c0e2c599d851d276c7e1b18dc
Felix Freiberger <felix@fefrei.de>
2014-06-27 17:57:49 +0200

    Stripped down keys for keyboard navigation.

--- js/impress.js
+++ js/impress.js
@@ -681,7 +681,7 @@
         
         // Prevent default keydown action when one of supported key is pressed.
         document.addEventListener("keydown", function ( event ) {
-            if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
+            if ( event.keyCode === 9 || event.keyCode == 32 || event.keyCode == 39 ) {
                 event.preventDefault();
             }
         }, false);
@@ -705,15 +705,10 @@
             if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
                 switch( event.keyCode ) {
                     case 33: // pg up
-                    case 37: // left
-                    case 38: // up
                              api.prev();
                              break;
                     case 9:  // tab
-                    case 32: // space
                     case 34: // pg down
-                    case 39: // right
-                    case 40: // down
                              api.next();
                              break;
                 }
